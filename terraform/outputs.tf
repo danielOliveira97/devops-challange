566dd1c902a99068385469ae369b5c7fe9e77501
Daniel Oliveira <danieliver2011@gmail.com>
2022-06-28 20:56:43 -0300

    feat: add outputs from vpc aws module

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,18 @@ output "caller_arn" {
 output "caller_user" {
   value = data.aws_caller_identity.current.user_id
 }
+
+output "vpc_private_subnet_cidr" {
+  description = "VPC private subnet CIDR"
+  value       = module.vpc.private_subnets_cidr_blocks
+}
+
+output "vpc_public_subnet_cidr" {
+  description = "VPC public subnet CIDR"
+  value       = module.vpc.public_subnets_cidr_blocks
+}
+
+output "vpc_cidr" {
+  description = "VPC CIDR"
+  value       = module.vpc.vpc_cidr_block
+}
