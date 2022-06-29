88a04a3ae3ee21d37762f252081d991e6248f889
Daniel Oliveira <danieliver2011@gmail.com>
2022-06-28 21:53:47 -0300

    feat: configure nginx ingress in aws eks

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,4 +66,10 @@ module "eks_blueprints_kubernetes_addons" {
   enable_aws_load_balancer_controller = true
   enable_cluster_autoscaler           = true
   enable_metrics_server               = true
+
+  enable_ingress_nginx = true
+  ingress_nginx_helm_config = {
+    version = "4.0.17"
+    values  = [templatefile("${path.module}/nginx_values.yaml", {})]
+  }
 }
